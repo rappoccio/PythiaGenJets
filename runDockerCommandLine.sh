45b2242eee5407332f60421d0ae4caff5c0d716f
Salvatore Rappoccio <rappoccio@gmail.com>
2018-09-12 15:42:16 -0400

    Adding command line option for the ROOT docker imaage

---
+++ runDockerCommandLine.sh
@@ -0,0 +1,3 @@
+ip=$(ifconfig en0 | grep inet | awk '$1=="inet" {print $2}')
+xhost + $ip
+docker run --rm -it -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=$ip:0 --entrypoint /bin/bash srappoccio/fastjet-tutorial-root:latest
