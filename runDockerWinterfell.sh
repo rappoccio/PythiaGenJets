218c4c21496ae8732e08e193f220b9f982e2434f
Salvatore Rappoccio <rappoccio@gmail.com>
2019-11-06 12:05:15 -0500

    Simplify access to pythia gen jets

--- runDockerWinterfell.sh
+++ runDockerWinterfell.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-docker run --rm -it -v /tmp/.X11-unix:/tmp/.X11-unix -v ${PWD}:/home/physicist/results -e DISPLAY=$ip:0 --entrypoint /bin/bash $1
+docker run --rm -it -v /tmp/.X11-unix:/tmp/.X11-unix -v ${PWD}:/home/physicist/ -e DISPLAY=$ip:0 --entrypoint /bin/bash $1
